e68344a6c14bf6eefd239527fec373882befe454
benjawan.ru <benjawan.ru@tnis.com>
2023-12-15 19:38:15 +0700

    [feat] add rout

--- internal/controller/rest/routes.go
+++ internal/controller/rest/routes.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/neversitup-test/internal/controller/rest/odd"
 	"github.com/neversitup-test/internal/controller/rest/shufflings"
 )
 
@@ -13,6 +14,7 @@ func Load() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/shufflings", shufflings.Handle).Methods("POST")
+	router.HandleFunc("/odd-number", odd.Handle).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
